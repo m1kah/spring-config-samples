028b1c5584a684f534b03ad77ee2ab9d6d118394
Mika Hamalainen <mika.hamalainen@gmail.com>
2020-09-21 20:19:09 +0300

    Initial commit

---
+++ src/main/java/com/mika/App.java
@@ -0,0 +1,12 @@
+package com.mika;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        run(App.class, args);
+    }
+}
